e5aa00971dfb33daf500eadcb8e378629daf5ace
mrcc87 <mrcc87@gmail.com>
2015-05-18 00:15:58 +0200

    Added todo items edit specs and code

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -10,7 +10,7 @@ class TodoItemsController < ApplicationController
 
   def create
     @todo_list = TodoList.find(params[:todo_list_id])
-    @todo_item = @todo_list.todo_items.new(todo_items_params)
+    @todo_item = @todo_list.todo_items.new(todo_item_params)
     if @todo_item.save
       flash[:success] = "Added todo list item."
       redirect_to todo_list_todo_items_path
@@ -20,8 +20,29 @@ class TodoItemsController < ApplicationController
     end
   end
 
+  def edit
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.find(params[:id])
+  end
+
+  def update
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.update_attributes(todo_item_params)
+      flash[:success] = "Saved todo list item."
+      redirect_to todo_list_todo_items_path
+    else
+      flash[:error] = "That todo item could not be saved."
+      render action: :edit
+    end
+  end
+
+  def url_options
+    { todo_list_id: params[:todo_list_id] }.merge super
+  end
+
   private
-  def todo_items_params
+  def todo_item_params
     params[:todo_item].permit(:content)
   end
 end
